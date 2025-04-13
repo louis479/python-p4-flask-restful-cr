4a365d7bd5b8d1e8f4188512aa0406c28f57c080
louis-ngugi <louisngugi858@gmail.com>
2025-04-13 18:53:51 +0300

    learnt about RESTful API

--- server/app.py
+++ server/app.py
@@ -17,7 +17,49 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+    def get(self):
+        newsletters = Newsletter.query.all()
+        if not newsletters:
+            return {"message": "No newsletters found."}, 
+        response_dict_list = [n.to_dict() for n in newsletters]
+        return response_dict_list, 200
+    
+    def post(self):
+        new_record = Newsletter(
+            title=request.form['title'],
+            body=request.form['body']
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        return new_record.to_dict(), 201
+
+api.add_resource(Newsletters, '/newsletters')
+
+
+class NewsletterByID(Resource):
+    def get(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        return newsletter.to_dict(), 200
+    
+api.add_resource(NewsletterByID, '/newsletters/<int:id>') 
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
